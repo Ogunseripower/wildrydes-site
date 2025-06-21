825eeedc9539b316e3b5b35aaa5d24964e297c8e
Ogunseripower <40373162+Ogunseripower@users.noreply.github.com>
2025-06-20 22:36:19 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_0v3Rhpfap', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '279ratcvl0pjc9njjhk9v21vg9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        clientSecret: 'jfisjo6l1833nl3ll1gemca3rvfj8mhded42crj08pviqu4d0vt'
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
